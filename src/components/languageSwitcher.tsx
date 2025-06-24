e78a40137157e0fc1921eaba29da7b2365dbf530
achyutem <achyutem.ad@gmail.com>
2025-06-24 16:31:38 +0530

    language switcher

---
+++ src/components/languageSwitcher.tsx
@@ -0,0 +1,17 @@
+import { UseLanguage } from "@/context/languageContext";
+
+const LanguageToggle = () => {
+  const { language, toggleLanguage } = UseLanguage();
+
+  return (
+    <button
+      onClick={toggleLanguage}
+      className="fixed top-4 right-4 px-4 py-2 text-sm bg-orange-500 hover:bg-orange-700 transform transition duration-300 hover:scale-105 rounded-md font-medium text-white z-50"
+      aria-label="Toggle language"
+    >
+      {language === "english" ? "Hindi" : "English"}
+    </button>
+  );
+};
+
+export default LanguageToggle;
