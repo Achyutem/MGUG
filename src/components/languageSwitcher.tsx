ea07fab98fe4c4c604906ba13eee78a0851ca527
achyutem <achyutem.ad@gmail.com>
2025-06-25 16:54:48 +0530

    language button style

--- src/components/languageSwitcher.tsx
+++ src/components/languageSwitcher.tsx
@@ -4,14 +4,32 @@ const LanguageToggle = () => {
   const { language, toggleLanguage } = UseLanguage();
 
   return (
-    <button
+    <span
       onClick={toggleLanguage}
-      className="fixed top-4 right-4 px-4 py-2 text-sm bg-orange-500 hover:bg-orange-700 transform transition duration-300 hover:scale-105 rounded-md font-medium text-white z-50"
+      className="cursor-pointer underline text-sm text-white hover:text-orange-400 transition-colors"
       aria-label="Toggle language"
     >
-      {language === "english" ? "Hindi" : "English"}
-    </button>
+      {language === "english" ? "हिन्दी" : "English"}
+    </span>
   );
 };
 
 export default LanguageToggle;
+
+// import { UseLanguage } from "@/context/languageContext";
+
+// const LanguageToggle = () => {
+//   const { language, toggleLanguage } = UseLanguage();
+
+//   return (
+//     <button
+//       onClick={toggleLanguage}
+//       className="fixed top-4 right-4 px-4 py-2 text-sm bg-orange-500 hover:bg-orange-700 transform transition duration-300 hover:scale-105 rounded-md font-medium text-white z-50"
+//       aria-label="Toggle language"
+//     >
+//       {language === "english" ? "Hindi" : "English"}
+//     </button>
+//   );
+// };
+
+// export default LanguageToggle;
