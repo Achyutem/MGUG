0bf615fbe99734eec8e4751b97734e0b9848d1a7
achyutem <achyutem.ad@gmail.com>
2025-06-25 16:59:51 +0530

    footer data for language switch

---
+++ src/utils/footerData.ts
@@ -0,0 +1,24 @@
+export const translations = {
+  hindi: {
+    universityName: "महायोगी गोरखनाथ विश्वविद्यालय गोरखपुर",
+    tagline: "शिक्षा का पुनर्परिभाषन",
+    followUs: "हमें फॉलो करें",
+    contact: "संपर्क करें",
+    importantLinks: "महत्वपूर्ण लिंक",
+    policyAndReports: "नीति और रिपोर्ट",
+    copyright: "© 2025 महायोगी गोरखनाथ विश्वविद्यालय। सर्वाधिकार सुरक्षित।",
+    address: "आरोग्यधाम, बलापार रोड, सोनबरसा, गोरखपुर-273007, उत्तर प्रदेश",
+  },
+  english: {
+    universityName: "Mahayogi Gorakhnath University Gorakhpur",
+    tagline: "Redefining Education",
+    followUs: "Follow Us",
+    contact: "Contact",
+    importantLinks: "Important Links",
+    policyAndReports: "Policy & Reports",
+    copyright:
+      "© 2025 Mahayogi Gorakhnath University Gorakhpur. All rights reserved.",
+    address:
+      "Arogyadham, Balapar Road, Sonbarsa, Gorakhpur-273007, Uttar Pradesh",
+  },
+};
